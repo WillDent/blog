9aeb56615baf11a3a77fe61b03584ac393e5d73e
Will Dent <will.dent@gmail.com>
2024-04-03 11:40:36 -0400

    Initial Updates

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
+  
+      <div className="mt-8"></div>
+      <section>
+        <h1>Will Dent</h1>
+        <p>Creative Technologist</p>
+      </section>
       {allPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
